193b765d4971af9a55f2f2e2fb6914a8757c9165
Dennys Fredericci <dennys.fredericci@kbc.be>
2019-10-20 22:50:44 +0200

    HHH-13615 - Add unit test to reproduce error when tables are created in wrong schemas and specifying entity catalog/schema

--- orm/hibernate-orm-5/src/test/java/org/hibernate/bugs/ORMUnitTestCase.java
+++ orm/hibernate-orm-5/src/test/java/org/hibernate/bugs/ORMUnitTestCase.java
@@ -17,10 +17,27 @@ package org.hibernate.bugs;
 
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.model.relational.Database;
+import org.hibernate.boot.model.relational.Namespace;
+import org.hibernate.boot.registry.BootstrapServiceRegistry;
+import org.hibernate.boot.registry.BootstrapServiceRegistryBuilder;
+import org.hibernate.bugs.entities.AnEntity;
+import org.hibernate.bugs.entities.EntityA;
+import org.hibernate.bugs.entities.EntityB;
 import org.hibernate.cfg.AvailableSettings;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.engine.spi.SessionFactoryImplementor;
+import org.hibernate.mapping.Table;
+import org.hibernate.service.spi.SessionFactoryServiceRegistry;
 import org.hibernate.testing.junit4.BaseCoreFunctionalTestCase;
 import org.junit.Test;
+
+import java.util.stream.StreamSupport;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 /**
  * This template demonstrates how to develop a test case for Hibernate ORM, using its built-in unit test framework.
@@ -37,8 +54,9 @@ public class ORMUnitTestCase extends BaseCoreFunctionalTestCase {
 	@Override
 	protected Class[] getAnnotatedClasses() {
 		return new Class[] {
-//				Foo.class,
-//				Bar.class
+				AnEntity.class,
+				EntityA.class,
+				EntityB.class
 		};
 	}
 
@@ -53,7 +71,16 @@ public class ORMUnitTestCase extends BaseCoreFunctionalTestCase {
 	// If those mappings reside somewhere other than resources/org/hibernate/test, change this.
 	@Override
 	protected String getBaseForMappings() {
-		return "org/hibernate/test/";
+		return "org/hibernate/bugs/entities";
+	}
+
+	@Override
+	protected BootstrapServiceRegistry buildBootstrapServiceRegistry() {
+		final BootstrapServiceRegistryBuilder builder = new BootstrapServiceRegistryBuilder();
+		builder.applyIntegrator(MetadataExtractorIntegrator.INSTANCE);
+		builder.applyClassLoader( getClass().getClassLoader() );
+		prepareBootstrapRegistryBuilder( builder );
+		return builder.build();
 	}
 
 	// Add in any settings that are specific to your test.  See resources/hibernate.properties for the defaults.
@@ -68,12 +95,44 @@ public class ORMUnitTestCase extends BaseCoreFunctionalTestCase {
 
 	// Add your tests, using standard JUnit.
 	@Test
-	public void hhh123Test() throws Exception {
-		// BaseCoreFunctionalTestCase automatically creates the SessionFactory and provides the Session.
-		Session s = openSession();
-		Transaction tx = s.beginTransaction();
-		// Do stuff...
-		tx.commit();
-		s.close();
+	public void hhh13615() {
+
+		Namespace schemaSecondary = StreamSupport.stream(MetadataExtractorIntegrator.INSTANCE.getDatabase().getNamespaces().spliterator(), false)
+				.filter(namespace -> namespace.getName().getSchema().getText().equals("schema_secondary"))
+				.findAny()
+				.orElse(null);
+
+		assertNotNull(schemaSecondary);
+		assertArrayEquals(new String[]{"EntityA", "EntityB", "EntityA_EntityB", "EntityB_EntityA"}, schemaSecondary.getTables().stream().map(Table::getName).toArray(String[]::new));
+		assertEquals(4, schemaSecondary.getTables().size());
+	}
+
+
+	static class MetadataExtractorIntegrator implements org.hibernate.integrator.spi.Integrator {
+
+		public static final MetadataExtractorIntegrator INSTANCE = new MetadataExtractorIntegrator();
+
+		private Database database;
+
+		public Database getDatabase() {
+			return database;
+		}
+
+		@Override
+		public void integrate(
+				Metadata metadata,
+				SessionFactoryImplementor sessionFactory,
+				SessionFactoryServiceRegistry serviceRegistry) {
+
+			database = metadata.getDatabase();
+		}
+
+		@Override
+		public void disintegrate(
+				SessionFactoryImplementor sessionFactory,
+				SessionFactoryServiceRegistry serviceRegistry) {
+		}
 	}
 }
+
+
